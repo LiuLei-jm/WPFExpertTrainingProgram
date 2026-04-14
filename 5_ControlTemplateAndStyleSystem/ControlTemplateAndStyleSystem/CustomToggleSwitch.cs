80765fa2e4dfa155a35baf5ce6cefea2874ff6df
LiuLei-jm <14699396@qq.com>
2026-04-14 10:13:26 +0800

    Phase 5

---
+++ 5_ControlTemplateAndStyleSystem/ControlTemplateAndStyleSystem/CustomToggleSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ControlTemplateAndStyleSystem;
+
+[TemplatePart(Name = PartThumbName, Type = typeof(Ellipse))]
+public class CustomToggleSwitch : ToggleButton
+{
+    private const string PartThumbName = "PART_Thumb";
+    private Ellipse _thumbElement;
+
+
+    public Brush CheckedBackground
+    {
+        get { return (Brush)GetValue(CheckedBackgroundProperty); }
+        set { SetValue(CheckedBackgroundProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for CheckedBackground.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty CheckedBackgroundProperty =
+        DependencyProperty.Register(nameof(CheckedBackground), typeof(Brush), typeof(CustomToggleSwitch), new PropertyMetadata(Brushes.LimeGreen));
+
+
+    static CustomToggleSwitch()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomToggleSwitch), new FrameworkPropertyMetadata(typeof(CustomToggleSwitch)));
+    }
+    public override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        if (_thumbElement != null) _thumbElement.MouseEnter -= Thumb_MouseEnter;
+        _thumbElement = GetTemplateChild(PartThumbName) as Ellipse;
+        if (_thumbElement != null) _thumbElement.MouseEnter += Thumb_MouseEnter;
+    }
+
+    private void Thumb_MouseEnter(object sender, MouseEventArgs e)
+    {
+    }
+}
---
+++ 5_ControlTemplateAndStyleSystem/ControlTemplateAndStyleSystem/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace ControlTemplateAndStyleSystem;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private bool _isWifiEnabled = false;
+    public bool IsWifiEnabled
+    {
+        get => _isWifiEnabled;
+        set
+        {
+            if (_isWifiEnabled != value)
+            {
+                _isWifiEnabled = value;
+                OnPropertyChanged(nameof(IsWifiEnabled));
+            }
+        }
+    }
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.DataContext = this;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
