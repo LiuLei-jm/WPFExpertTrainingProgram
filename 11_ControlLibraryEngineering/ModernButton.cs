f6073da560ff4c2965f7ccfc9a136777d02e3824
LiuLei-jm <14699396@qq.com>
2026-04-29 17:05:44 +0800

    Phase 11

---
+++ 11_ControlLibraryEngineering/ModernButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlLibraryEngineering;
+
+public class ModernButton : Button
+{
+    static ModernButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(ModernButton),
+            new FrameworkPropertyMetadata(typeof(ModernButton))
+            );
+    }
+    public CornerRadius CornerRadius
+    {
+        get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+        set { SetValue(CornerRadiusProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(ModernButton), new PropertyMetadata(new CornerRadius(5)));
+
+}
